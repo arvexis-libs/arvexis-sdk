b11960dc6de0c753fde3e8e5c59f2f09225eb2e3
JRframe <1042785075@qq.com>
2025-05-30 17:44:03 +0800

    add trezor device pairing flow with usb/hid detection

--- video-module/internal/NativeVideoCom.ts
+++ video-module/internal/NativeVideoCom.ts
@@ -1,20 +1,18 @@
 import { _decorator, Component, Node } from 'cc';
 import { VideoCom } from '../VideoCom';
-import { EVideoType, IVideoParam } from '../VideoEnum';
 import { Sprite } from 'cc';
 import { VideoPlayer } from 'cc';
+import { EVideoType, IVideoParam } from '../VideoEnum';
 import { SpriteFrame } from 'cc';
 import { screen } from 'cc';
 import { UITransform } from 'cc';
 import { Vec3 } from 'cc';
-import { view } from 'cc';
-import { oops } from '../../../../../extensions/oops-plugin-framework/assets/core/Oops';
+import { oops } from 'db://oops-framework/core/Oops';
 import { VideoClip } from 'cc';
 const { ccclass, property } = _decorator;
 
-@ccclass('DefaultVideoCom')
-export class DefaultVideoCom extends VideoCom {
-
+@ccclass('NativeVideoCom')
+export class NativeVideoCom extends VideoCom {
     @property(Sprite)
     videoSprite: Sprite = null!;
 
@@ -27,17 +25,8 @@ export class DefaultVideoCom extends VideoCom {
         this.mVideoPlayer.node.on(VideoPlayer.EventType.STOPPED, this.onStopped, this);
         this.mVideoPlayer.node.on(VideoPlayer.EventType.COMPLETED, this.onCompleted, this);
 
-        const VideoWidth = oops.gui.root.w;
-        const VideoHeight = oops.gui.root.h;
-
-        const width = 1080;
-        const height = 1920;
-
-        this.videoSprite.node.scale_x = VideoWidth / width;
-        this.videoSprite.node.scale_y = VideoHeight / height;
-        const uiTransform = this.videoSprite.getComponent(UITransform)!;
-        uiTransform.width = width;
-        uiTransform.height = height;
+        this.mPosterWidgetHigh = true;
+        this.fixPosterSprite();
     }
 
     play(param: IVideoParam): void {
@@ -46,7 +35,7 @@ export class DefaultVideoCom extends VideoCom {
         this.videoSprite.node.active = false;
         let poster = param.poster ? param.poster : null;
         if(param.poster === "" || param.poster === null || param.poster === undefined){
-
+            console.log(`[video] , , poster: ${param.poster}`);
             this.videoSprite.node.active = false;
         }
         else{
@@ -58,36 +47,52 @@ export class DefaultVideoCom extends VideoCom {
                 }
             });
         }
-        this.mPosterWidgetHigh = true;
-        //this.fixPosterSprite();
+
 
         this.mVideoPlayer.fullScreenOnAwake = true;
         this.mVideoPlayer.resourceType = param.resourceType == EVideoType.Remote ? VideoPlayer.ResourceType.REMOTE : VideoPlayer.ResourceType.LOCAL;
+        // this.mVideoPlayer.resourceType = VideoPlayer.ResourceType.REMOTE;
         if(param.resourceType == EVideoType.Local){
 
+            console.log(`[video] , src: ${param.src}`);
             this.loadAsync("InnerVideo", param.src, VideoClip).then((clip)=>{
                 this.mVideoPlayer.clip = clip;
             })
         }
         else{
+            console.log(`[video] , src: ${param.src}`);
             this.mVideoPlayer.remoteURL = param.src;
         }
-
         this.mVideoPlayer.loop = param.loop;
+        this.mVideoPlayer
+
         
     }
 
     onReadyToPlay(){
 
-        console.log("[video] DefaultVideoCom ");
-        const VideoWidth = oops.gui.root.w;
-        const VideoHeight = oops.gui.root.h;
+        console.log("[video] NativeVideoCom ");
+        const VideoWidth = 1080;
+        const VideoHeight = 1920;
         super.onReadyToPlay();
+
         this.videoSprite.node.active = false;
-        
+
+        const width = screen.windowSize.width;
+        const height = screen.windowSize.height;
+        const uiTransform = this.videoSprite.getComponent(UITransform)!;
+        uiTransform.width = VideoWidth;
+        uiTransform.height = VideoHeight;
+        this.videoSprite.node.position = Vec3.ZERO;
         this.mVideoPlayer.play();
-        //let scaleRate = height / VideoHeight;
+        let scaleRate = height / VideoHeight;
         //this.videoSprite.node.scale = new Vec3(scaleRate,scaleRate,scaleRate);
+    }
+
+    stop(){
+        super.stop();
+        this.mVideoPlayer.stop();
+        this.onStopped();
     }
 
     onStopped(): void {
@@ -96,24 +101,6 @@ export class DefaultVideoCom extends VideoCom {
 
     onCompleted(): void {
         super.onCompleted();
-        this.mVideoPlayer.stop();
-    }
-
-    stop(): void {
-        super.stop();
-        this.mVideoPlayer.stop();
-    }
-
-    seek(time: number): void {
-        this.mVideoPlayer.currentTime = time;
-    }
-
-    getDuration(): number{
-        return this.mVideoPlayer.duration;
-    }
-
-    getCurrentTime(): number{
-        return this.mVideoPlayer.currentTime;
     }
 
 
@@ -127,9 +114,11 @@ export class DefaultVideoCom extends VideoCom {
 
     fixPosterSprite(){
         if(this.mPosterWidgetHigh){
-            const ws = screen.windowSize;
-            console.log("windowSize ==== " + ws.width + " " + ws.height);
-            let atio = ws.height / ws.width;
+            const screenW = oops.gui.root.w;
+            const screenH = oops.gui.root.h;
+
+            console.log("windowSize ==== " + screenW + " " + screenH);
+            let atio = screenH / screenW;
             let scale = Math.abs(atio - 16/9) + 1;
             console.log("scale ==== " + scale);
             this.videoSprite.node.scale = new Vec3(scale, scale, scale);
@@ -145,6 +134,19 @@ export class DefaultVideoCom extends VideoCom {
         this.mVideoPlayer.node.off(VideoPlayer.EventType.COMPLETED, this.onCompleted, this);
         super.onDestroy();
     }
+
+    seek(time: number): void {
+        this.mVideoPlayer.currentTime = time;
+    }
+    
+    getDuration(): number{
+        return this.mVideoPlayer.duration;
+    }
+
+    getCurrentTime(): number{
+        return this.mVideoPlayer.currentTime;
+    }
+
 }
 
 
